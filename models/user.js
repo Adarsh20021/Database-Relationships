ddbd0b3d836e2943dd33d28fc9dfb5364993c44c
Adarsh20021 <adarshkulkarni10@gmail.com>
2026-07-28 11:32:51 +0530

    One to Few Implementation in mongoose

---
+++ models/user.js
@@ -0,0 +1,37 @@
+// getting-started.js
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+main().then(()=>{console.log("Connection Successful!")}).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+}
+
+const userSchema = new Schema({
+    username: String,
+    addresses: [
+        {
+            _id: false,             //if we dont want mongoose to give ids for each location(both locations will come under a single _id).
+            location: String,
+            city: String,
+        },
+    ],
+});
+
+const User = mongoose.model("User",userSchema);
+
+const addUsers = async()=>{
+    let user1 = new User({
+        username: "SherlockHolmes",
+        addresses: [{
+            location: "221B Baker Street",
+            city: "London"
+        }]
+    });
+    user1.addresses.push({location: "P32 WallStreet",city: "London"});
+    let result = await user1.save();
+    console.log(result);
+}
+
+addUsers();
